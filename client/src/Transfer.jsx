77c1304a9d8ee10a549a3a3123f4c8c068cb0e70
vincent <jacky.vincent095@gmail.com>
2024-06-19 12:28:35 +0800

    update

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import server from "./server";
+import {keccak256} from "ethereum-cryptography/keccak";
+import {secp256k1} from "ethereum-cryptography/secp256k1"
+import {utf8ToBytes, toHex } from "ethereum-cryptography/utils"
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
+
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
@@ -11,12 +16,24 @@ function Transfer({ address, setBalance }) {
     evt.preventDefault();
 
     try {
+            // // sign message hash
+      const message =  {
+        sender: address,
+        amount: parseInt(sendAmount),
+        recipient: recipient,
+      };
+      const messageHash = keccak256(utf8ToBytes(JSON.stringify(message)));
+      console.log('messageHash', messageHash);
+      console.log('priv', privateKey);
+      const signature = secp256k1.sign(messageHash, privateKey);
+      console.log('signature', signature);
+
       const {
         data: { balance },
       } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
+        message,
+        messageHash,
+        signature
       });
       setBalance(balance);
     } catch (ex) {
@@ -43,6 +60,15 @@ function Transfer({ address, setBalance }) {
           placeholder="Type an address, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <label>
+        private key
+        <input
+          placeholder="Type privatekey, only used for generate signature"
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}
         ></input>
       </label>
 
--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const fs = require('fs');
 const app = express();
 const cors = require("cors");
+const {secp256k1}  = require('ethereum-cryptography/secp256k1');
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
-};
+const balances = JSON.parse(fs.readFileSync("./balances.json", 'utf-8'));
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
@@ -19,11 +17,24 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { message, messageHash, signature } = req.body;
+  // console.log('messageHash', messageHash);
+  // console.log('sig', typeof signature, signature);
+
+  // check wehther signature is signed by sender
+  const sender = message.sender;
+  const recoveredMessageHash = new Uint8Array(Object.values(messageHash)); // this is because messageHash turns to a list of k:v pair format
+  const recoveredSignature = {'r': BigInt(signature.r), 's': BigInt(signature.s), 'recovery': signature.recovery};
+  const isSigned = secp256k1.verify(recoveredSignature, recoveredMessageHash, message.sender);
+  if (!isSigned) {
+    res.status(400).send({message: 'Not signed by sender!'});
+  }
+  const recipient = message.recipient;
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
+  amount = message.amount;
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
